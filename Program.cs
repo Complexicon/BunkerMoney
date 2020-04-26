daa21d728efb9b53abe48864d471a9d35babf3f8
Philipp Leer <philippleer@gmail.com>
2020-04-26 22:23:55 +0200

    MAJOR MAKEOVER Drop Forms Once and for all Make fully idiotproof -- okay more like 96% cus retards gon find a way to fuck it over added helper functions: godmode wanted level removal teleport objective (for obvious reasons useful) teleport waypoint set explosive ammo for current weapon Now featuring audiophile eargasm audio feedback did i forget something? idk check code yrself

--- Program.cs
+++ Program.cs
@@ -1,68 +1,288 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
-using static System.Windows.Forms.DialogResult;
 
 namespace BunkerMoney {
 	static class Program {
 
-		static int[] bunkerOff = new int[] { 0x1180, 0x41B8 };
+		static long WorldPTR;
 		static long GlobalPTR;
-		static int limit = 28;
+		static long BlipPTR;
+
+		/*
+		THIS IS IMPORTANT! limit is the hardcap 25 means 2.5mil (100k * 25)
+		*/
+		static int limit = 25;
+		//bonus compensation is hardcoded for now
+		//i dont have the offset to get player count and i wont fuck around to find it
+		static double bonusCompensation = 0.25;
+		//stores last known value in bunker
+		static int lastMoneyInBunker = 0;
 
 		static Mem Mem;
+		static MenuItem debug;
+		static NotifyIcon trayIcon;
 
-		static string url = "https://raw.githubusercontent.com/Complexicon/BunkerMoney/master/offsets.ini";
+		//ASYNCKEYSTATE
+		[DllImport("user32.dll")]
+		public static extern int GetAsyncKeyState(Keys i);
 
 		[STAThread]
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			using(WebClient client = new WebClient()) {
-				try {
-					var cfgDict = new Dictionary<string, int>();
-					string s = client.DownloadString(url);
-					string[] lines = s.Split(Environment.NewLine.ToCharArray());
-					Console.WriteLine(lines);
-					foreach(string i in lines) {
-						if(i == "") continue;
-						var splitted = i.Split('=');
-						cfgDict[splitted[0]] = Convert.ToInt32(splitted[1], 16);
-					}
-					bunkerOff[0] = cfgDict["offset1"];
-					bunkerOff[1] = cfgDict["offset2"];
-					limit = cfgDict["limit"];
-				} catch {
-					MessageBox.Show("Couldn't load Remote Offsets! Using local Offsets (GTA 1.50)!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-				}
-			}
+			ContextMenu trayMenu = new ContextMenu();
+
+			//why did i add a tray icon?
+			trayIcon = new NotifyIcon {
+				Text = "BunkerMoney",
+				Icon = BunkerMoney.Properties.Resources.icon,
+				ContextMenu = trayMenu,
+				Visible = true
+			};
+			debug = trayMenu.MenuItems.Add("lastMoneyInBunker: 0");
+			trayMenu.MenuItems.Add("-");
+			trayMenu.MenuItems.Add("Exit", Exit);
 
 			try {
 				Mem = new Mem("GTA5");
-				var addr = Mem.FindPattern(new byte[] { 0x4C, 0x8D, 0x05, 0x0, 0x0, 0x0, 0x0, 0x4D, 0x8B, 0x08, 0x4D, 0x85, 0xC9, 0x74, 0x11 }, "xxx????xxxxxxxx");
-				GlobalPTR = addr + Mem.ReadInt(addr + 3, null) + 7;
+				long addr = Mem.FindPattern(new byte[] { 0x48, 0x8B, 0x05, 0x0, 0x0, 0x0, 0x0, 0x45, 0x0, 0x0, 0x0, 0x0, 0x48, 0x8B, 0x48, 0x08, 0x48, 0x85, 0xC9, 0x74, 0x07 }, "xxx????x????xxxxxxxxx");
+				WorldPTR = addr + Mem.ReadInt(addr + 3, null) + 7;
+				long addr2 = Mem.FindPattern(new byte[] { 0x4C, 0x8D, 0x05, 0x0, 0x0, 0x0, 0x0, 0x0F, 0xB7, 0xC1 }, "xxx????xxx");
+				BlipPTR = addr2 + Mem.ReadInt(addr2 + 3, null) + 7;
+				var addr3 = Mem.FindPattern(new byte[] { 0x4C, 0x8D, 0x05, 0x0, 0x0, 0x0, 0x0, 0x4D, 0x8B, 0x08, 0x4D, 0x85, 0xC9, 0x74, 0x11 }, "xxx????xxxxxxxx");
+				GlobalPTR = addr3 + Mem.ReadInt(addr3 + 3, null) + 7;
 			} catch {
 				MessageBox.Show("GTA is not Running!", "Serious Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
 			}
 
-			var priceDiag = new PriceDialog();
-			var amtDiag = new AmtDialog();
-
-			if(priceDiag.ShowDialog() == OK && amtDiag.ShowDialog() == OK) {
-				try {
-					Mem.WriteInt(GlobalPTR - 0x128, bunkerOff, Convert.ToInt32((100000 / (priceDiag.price / amtDiag.amt)) * limit));
-					MessageBox.Show("Done! Deliver Vehlicle(s) and Enjoy :) Program will now close!", "Yay!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-				} catch { MessageBox.Show("There was an Error during the Procedure!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
-			} else { MessageBox.Show("Please make sure everything is correct!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+			new Thread(Thr).Start();
+			Application.Run();
 
 		}
+
+		private static void Thr() {
+			//god i hate this shit
+			while(true) {
+
+				if(GetAsyncKeyState(Keys.B) != 0) {
+
+					//READ last money -- todo: fix garbage data
+					if(PressedOnce(Keys.R)) {
+						lastMoneyInBunker = Mem.ReadInt(GlobalPTR - 0x128, Offset.moneyInBunker);
+						Toggle();
+						debug.Text = "lastMoneyInBunker: " + lastMoneyInBunker + "$";
+					}
+
+					//PATCH packages
+					if(PressedOnce(Keys.P)) {
+						int packetAmtForMil = Convert.ToInt32(100000 / ((lastMoneyInBunker + (lastMoneyInBunker * bonusCompensation)) / Mem.ReadInt(GlobalPTR - 0x128, Offset.packetMax)));
+						Mem.Write(GlobalPTR - 0x128, Offset.packet, packetAmtForMil * limit);
+
+						Activate();
+					}
+
+
+
+				}
+
+				//retarded ass "helper" functions
+				if(GetAsyncKeyState(Keys.X) != 0) {
+					if(PressedOnce(Keys.D1)) Teleport(ObjectiveCoords);
+					if(PressedOnce(Keys.D2)) Teleport(WaypointCoords);
+					if(PressedOnce(Keys.G)) {
+						if(Godmode) Deactivate();
+						else Activate();
+						Godmode = !Godmode;
+					}
+					if(PressedOnce(Keys.E)) {
+						if(WeaponBulletType == 3) {
+							WeaponBulletType = 5;
+							WeaponExplosionType = 18;
+							Activate();
+						} else {
+							WeaponBulletType = 3;
+							WeaponExplosionType = -1;
+							Deactivate();
+						}
+
+					}
+					if(PressedOnce(Keys.W)) {
+						WantedLevel = 0;
+						Activate();
+					}
+				}
+
+				ClearStates();
+				Thread.Sleep(10);
+			}
+		}
+
+		//audio response to actions
+		public static void Activate() {
+			Console.Beep(523, 75);
+			Console.Beep(587, 75);
+			Console.Beep(700, 75);
+		}
+
+		public static void Deactivate() {
+			Console.Beep(700, 75);
+			Console.Beep(587, 75);
+			Console.Beep(523, 75);
+		}
+
+		public static void Toggle() {
+			Console.Beep(523, 75);
+			Console.Beep(523, 75);
+			Console.Beep(523, 75);
+		}
+
+		private static void Exit(object sender, EventArgs e) {
+			trayIcon.Visible = false;
+			Environment.Exit(0);
+		}
+
+		//KEYBOARD HELPER
+
+		static List<Keys> pressedKeys = new List<Keys>();
+
+		static bool PressedOnce(Keys k) {
+			bool pressed = GetAsyncKeyState(k) > 0;
+			if(pressed && !pressedKeys.Contains(k)) {
+				pressedKeys.Add(k);
+				return true;
+			} else if(!pressed && pressedKeys.Contains(k)) pressedKeys.RemoveAll(item => item == k);
+			return false;
+		}
+
+		//flush keyboard state
+		//fucking slimmed down keyboard lib and mem lib coz vs19 cant link projects properly
+		static void ClearStates() {
+			for(int i=0; i < 0xFF; i++) GetAsyncKeyState((Keys)i);
+		}
+
+		//HAX VARS
+		public static int WeaponExplosionType {
+			get => Mem.ReadInt(WorldPTR, Offset.WeaponExplosionType);
+			set => Mem.Write(WorldPTR, Offset.WeaponExplosionType, (int)value);
+		}
+
+		public static int WeaponBulletType {
+			get => Mem.ReadInt(WorldPTR, Offset.WeaponDamageType);
+			set => Mem.Write(WorldPTR, Offset.WeaponDamageType, value);
+		}
+		public static int WantedLevel {
+			get => Mem.ReadInt(WorldPTR, Offset.WantedLevel);
+			set => Mem.Write(WorldPTR, Offset.WantedLevel, value);
+		}
+		public static bool Godmode {
+			get => Mem.ReadBool(WorldPTR, Offset.Godmode);
+			set => Mem.Write(WorldPTR, Offset.Godmode, value);
+		}
+		public static float PlayerX {
+			get => Mem.ReadFloat(WorldPTR, Offset.PlayerVecX);
+			set {
+				Mem.Write(WorldPTR, Offset.PlayerVecX, value);
+				Mem.Write(WorldPTR, Offset.PlayerX, value);
+			}
+		}
+
+		public static float PlayerY {
+			get => Mem.ReadFloat(WorldPTR, Offset.PlayerVecY);
+			set {
+				Mem.Write(WorldPTR, Offset.PlayerVecY, value);
+				Mem.Write(WorldPTR, Offset.PlayerY, value);
+			}
+		}
+
+		public static float PlayerZ {
+			get => Mem.ReadFloat(WorldPTR, Offset.PlayerVecZ);
+			set {
+				Mem.Write(WorldPTR, Offset.PlayerVecZ, value);
+				Mem.Write(WorldPTR, Offset.PlayerZ, value);
+			}
+		}
+
+		public static float CarX {
+			get => Mem.ReadFloat(WorldPTR, Offset.CarVecX);
+			set {
+				Mem.Write(WorldPTR, Offset.CarVecX, value);
+				Mem.Write(WorldPTR, Offset.CarX, value);
+			}
+		}
+
+		public static float CarY {
+			get => Mem.ReadFloat(WorldPTR, Offset.CarVecY);
+			set {
+				Mem.Write(WorldPTR, Offset.CarVecY, value);
+				Mem.Write(WorldPTR, Offset.CarY, value);
+			}
+		}
+
+		public static float CarZ {
+			get => Mem.ReadFloat(WorldPTR, Offset.CarVecZ);
+			set {
+				Mem.Write(WorldPTR, Offset.CarVecZ, value);
+				Mem.Write(WorldPTR, Offset.CarZ, value);
+			}
+		}
+
+		public static void Teleport(Location l) {
+			if(Mem.ReadInt(WorldPTR, Offset.PlayerInCar) == 256) {
+				CarX = l.x;
+				CarY = l.y;
+				CarZ = l.z;
+			} else {
+				PlayerX = l.x;
+				PlayerY = l.y;
+				PlayerZ = l.z;
+			}
+		}
+
+		//this coord shit is so hacky.
+
+		public static Location WaypointCoords {
+			get {
+				for(int i = 2000; i > 1; i--) {
+					if(Mem.ReadInt(BlipPTR + (i * 8), new int[] { 0x48 }) == 84 && Mem.ReadInt(BlipPTR + (i * 8), new int[] { 0x40 }) == 8) {
+						return new Location {
+							x = Mem.ReadFloat(BlipPTR + (i * 8), new int[] { 0x10 }),
+							y =Mem.ReadFloat(BlipPTR + (i * 8), new int[] { 0x14 }),
+							z = -210F
+						};
+					}
+				}
+				return new Location { x = PlayerX, y = PlayerY, z = PlayerZ };
+			}
+		}
+
+		public static Location ObjectiveCoords {
+			get {
+				for(int i = 2000; i > 1; i--) {
+					int objDetect = Mem.ReadInt(BlipPTR + (i * 8), new int[] { 0x48 });
+					if(Mem.ReadInt(BlipPTR + (i * 8), new int[] { 0x40 }) == 1 && ((objDetect == 1) || (objDetect == 66) || (objDetect == 60))) {
+						return new Location {
+							x = Mem.ReadFloat(BlipPTR + (i * 8), new int[] { 0x10 }),
+							y = Mem.ReadFloat(BlipPTR + (i * 8), new int[] { 0x14 }) + 10F,
+							z = Mem.ReadFloat(BlipPTR + (i * 8), new int[] { 0x18 })
+						};
+					}
+				}
+				return new Location { x = PlayerX, y = PlayerY, z = PlayerZ };
+			}
+		}
+
+
 	}
+
+	//VEC3 effectively
+	struct Location { public float x, y, z; }
 
 	class Mem {
 
@@ -82,8 +302,7 @@ namespace BunkerMoney {
 		}
 
 		public IntPtr GetProcHandle() {
-			try { return Proc.Handle; } 
-			catch { return IntPtr.Zero; }
+			try { return Proc.Handle; } catch { return IntPtr.Zero; }
 		}
 
 		public long GetPtrAddr(long Pointer, int[] Offset = null) {
@@ -106,37 +325,80 @@ namespace BunkerMoney {
 		public long FindPattern(byte[] pattern, string mask) {
 			int moduleSize = Proc.MainModule.ModuleMemorySize;
 
-			if(moduleSize == 0)
-				throw new Exception($"Size of module {Proc.MainModule.ModuleName} is INVALID.");
+			if(moduleSize == 0) throw new Exception($"Size of module {Proc.MainModule.ModuleName} is INVALID.");
 
 			byte[] moduleBytes = new byte[moduleSize];
 			ReadProcessMemory(GetProcHandle(), BaseAddress, moduleBytes, moduleSize);
 
 			for(long i = 0; i < moduleSize; i++) {
-				bool found = true;
+				for(int l = 0; l < mask.Length; l++)
+					//dirty hack heh
+					if(!(mask[l] == '?' || moduleBytes[l + i] == pattern[l])) goto SKIP;
 
-				for(int l = 0; l < mask.Length; l++) {
-					found = mask[l] == '?' || moduleBytes[l + i] == pattern[l];
-
-					if(!found)
-						break;
-				}
-
-				if(found) {
-					moduleBytes = null;
-					return i;
-				}
-
+				return i;
+				SKIP:;
 			}
 			return 0;
 		}
 
-		public void WriteInt(long BasePTR, int[] offset, int Value) => WriteProcessMemory(GetProcHandle(), GetPtrAddr(BaseAddress + BasePTR, offset), BitConverter.GetBytes(Value), BitConverter.GetBytes(Value).Length);
-		public int ReadInt(long BasePTR, int[] offset) {
-			byte[] Buffer = new byte[4];
-			ReadProcessMemory(GetProcHandle(), GetPtrAddr(BaseAddress + BasePTR, offset), Buffer, 4);
-			return BitConverter.ToInt32(Buffer, 0);
+
+		public byte[] ReadBytes(long BasePTR, int[] offset, int Length) {
+			byte[] Buffer = new byte[Length];
+			ReadProcessMemory(GetProcHandle(), GetPtrAddr(BaseAddress + BasePTR, offset), Buffer, Length);
+			return Buffer;
 		}
+
+		public void Write(long BasePTR, int[] offset, byte[] Bytes) => WriteProcessMemory(GetProcHandle(), GetPtrAddr(BaseAddress + BasePTR, offset), Bytes, Bytes.Length);
+
+		public void Write(long BasePTR, int[] offset, bool b) => Write(BasePTR, offset, b ? new byte[] { 0x01 } : new byte[] { 0x00 });
+		public void Write(long BasePTR, int[] offset, float Value) => Write(BasePTR, offset, BitConverter.GetBytes(Value));
+		public void Write(long BasePTR, int[] offset, double Value) => Write(BasePTR, offset, BitConverter.GetBytes(Value));
+		public void Write(long BasePTR, int[] offset, int Value) => Write(BasePTR, offset, BitConverter.GetBytes(Value));
+		public void Write(long BasePTR, int[] offset, string String) => Write(BasePTR, offset, new ASCIIEncoding().GetBytes(String));
+		public void Write(long BasePTR, int[] offset, long Value) => Write(BasePTR, offset, BitConverter.GetBytes(Value));
+		public void Write(long BasePTR, int[] offset, uint Value) => Write(BasePTR, offset, BitConverter.GetBytes(Value));
+		public void Write(long BasePTR, int[] offset, byte Value) => Write(BasePTR, offset, new byte[] { Value });
+		public bool ReadBool(long BasePTR, int[] offset) => ReadByte(BasePTR, offset) != 0x00;
+
+
+		public float ReadFloat(long BasePTR, int[] offset) => BitConverter.ToSingle(ReadBytes(BasePTR, offset, 4), 0);
+		public double ReadDouble(long BasePTR, int[] offset) => BitConverter.ToDouble(ReadBytes(BasePTR, offset, 8), 0);
+		public int ReadInt(long BasePTR, int[] offset) => BitConverter.ToInt32(ReadBytes(BasePTR, offset, 4), 0);
+		public uint ReadUInt(long BasePTR, int[] offset) => BitConverter.ToUInt32(ReadBytes(BasePTR, offset, 4), 0);
+		public string ReadString(long BasePTR, int[] offset, int size) => new ASCIIEncoding().GetString(ReadBytes(BasePTR, offset, size));
+		public long ReadPointer(long BasePTR, int[] offset) => BitConverter.ToInt64(ReadBytes(BasePTR, offset, 8), 0);
+		public byte ReadByte(long BasePTR, int[] offset) => ReadBytes(BasePTR, offset, 1)[0];
+
+	}
+
+	public static class Offset {
+
+		public static int[] packet = new int[] { 0x1180, 0x41B8 };
+		public static int[] packetMax = new int[] { 0x1180, 0x3A10 };
+		public static int[] moneyInBunker = new int[] { 0x1180, -0x3B770 };
+
+		public static int[] Godmode = new int[] { 0x08, 0x189 };
+
+		public static int[] WantedLevel = new int[] { 0x08, 0x10b8, 0x848 };
+
+		public static int[] PlayerX = new int[] { 0x08, 0x90 };
+		public static int[] PlayerY = new int[] { 0x08, 0x94 };
+		public static int[] PlayerZ = new int[] { 0x08, 0x98 };
+		public static int[] PlayerVecX = new int[] { 0x08, 0x30, 0x50 };
+		public static int[] PlayerVecY = new int[] { 0x08, 0x30, 0x54 };
+		public static int[] PlayerVecZ = new int[] { 0x08, 0x30, 0x58 };
+
+		public static int[] PlayerInCar = new int[] { 0x08, 0xE44 };
+
+		public static int[] CarX = new int[] { 0x08, 0xd28, 0x90 };
+		public static int[] CarY = new int[] { 0x08, 0xd28, 0x94 };
+		public static int[] CarZ = new int[] { 0x08, 0xd28, 0x98 };
+		public static int[] CarVecX = new int[] { 0x08, 0xd28, 0x30, 0x50 };
+		public static int[] CarVecY = new int[] { 0x08, 0xd28, 0x30, 0x54 };
+		public static int[] CarVecZ = new int[] { 0x08, 0xd28, 0x30, 0x58 };
+
+		public static int[] WeaponDamageType = new int[] { 0x08, 0x10C8, 0x20, 0x20 };
+		public static int[] WeaponExplosionType = new int[] { 0x08, 0x10C8, 0x20, 0x24 };
 
 	}
 
